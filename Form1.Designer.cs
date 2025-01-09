a3fc37f89925b9ad50e16d5ff53b59707e18b41f
deuswork <deuswork@DEUSWORK0898>
2025-01-09 13:35:42 +0100

    <PlatformTarget>AnyCPU</PlatformTarget>

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -109,7 +109,7 @@
             this.lblResult.Text = "Result";
 
             // Form1
-            this.ClientSize = new System.Drawing.Size(340, 175);
+            this.ClientSize = new System.Drawing.Size(340, 178);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.textBoxDinner);
