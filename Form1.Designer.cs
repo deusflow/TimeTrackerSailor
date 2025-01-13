b79003882ff3fb30ef6514b6da8dc9d749305ebe
deuswork <deuswork@DEUSWORK0898>
2025-01-13 08:37:54 +0100

    new comment, text errors have been corrected

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,128 +2,128 @@
 {
     partial class Form1
     {
-        private System.ComponentModel.IContainer components = null;
-        private DateTimePicker dateTimePickerStart;
-        private DateTimePicker dateTimePickerEnd;
-        private CheckBox checkBoxBreakfast;
-        private CheckBox checkBoxLunch;
-        private CheckBox checkBoxDinner;
-        private TextBox textBoxBreakfast;
-        private TextBox textBoxLunch;
-        private TextBox textBoxDinner;
-        private Button btnCalculate;
-        private Label lblResult;
+        private System.ComponentModel.IContainer components = null; // Container for components
+        private DateTimePicker dateTimePickerStart; // Start time picker
+        private DateTimePicker dateTimePickerEnd; // End time picker
+        private CheckBox checkBoxBreakfast; // Checkbox for breakfast break
+        private CheckBox checkBoxLunch; // Checkbox for lunch break
+        private CheckBox checkBoxDinner; // Checkbox for dinner break
+        private TextBox textBoxBreakfast; // Input for breakfast duration
+        private TextBox textBoxLunch; // Input for lunch duration
+        private TextBox textBoxDinner; // Input for dinner duration
+        private Button btnCalculate; // Button to start calculation
+        private Label lblResult; // Label to display results
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            if (disposing && (components != null)) // Check if resources need to be disposed
             {
-                components.Dispose();
+                components.Dispose(); // Dispose components
             }
-            base.Dispose(disposing);
+            base.Dispose(disposing); // Call base Dispose method
         }
 
         private void InitializeComponent()
         {
-            this.dateTimePickerStart = new DateTimePicker();
-            this.dateTimePickerEnd = new DateTimePicker();
-            this.checkBoxBreakfast = new CheckBox();
-            this.checkBoxLunch = new CheckBox();
-            this.checkBoxDinner = new CheckBox();
-            this.textBoxBreakfast = new TextBox();
-            this.textBoxLunch = new TextBox();
-            this.textBoxDinner = new TextBox();
-            this.btnCalculate = new Button();
-            this.lblResult = new Label();
+            this.dateTimePickerStart = new DateTimePicker(); // Initialize start time picker
+            this.dateTimePickerEnd = new DateTimePicker(); // Initialize end time picker
+            this.checkBoxBreakfast = new CheckBox(); // Initialize breakfast checkbox
+            this.checkBoxLunch = new CheckBox(); // Initialize lunch checkbox
+            this.checkBoxDinner = new CheckBox(); // Initialize dinner checkbox
+            this.textBoxBreakfast = new TextBox(); // Initialize breakfast input
+            this.textBoxLunch = new TextBox(); // Initialize lunch input
+            this.textBoxDinner = new TextBox(); // Initialize dinner input
+            this.btnCalculate = new Button(); // Initialize start button
+            this.lblResult = new Label(); // Initialize result label
 
-            this.SuspendLayout();
+            this.SuspendLayout(); // Temporarily suspend layout for performance
 
             // dateTimePickerStart
-            this.dateTimePickerStart.Anchor = (AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right);
-            this.dateTimePickerStart.Format = DateTimePickerFormat.Time;
-            this.dateTimePickerStart.Location = new System.Drawing.Point(12, 12);
-            this.dateTimePickerStart.Name = "dateTimePickerStart";
-            this.dateTimePickerStart.Size = new System.Drawing.Size(300, 250);
+            this.dateTimePickerStart.Anchor = (AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right); // Anchor to top and stretch horizontally
+            this.dateTimePickerStart.Format = DateTimePickerFormat.Time; // Use time format
+            this.dateTimePickerStart.Location = new System.Drawing.Point(12, 12); // Set position
+            this.dateTimePickerStart.Name = "dateTimePickerStart"; // Assign name
+            this.dateTimePickerStart.Size = new System.Drawing.Size(300, 250); // Set size
 
             // dateTimePickerEnd
-            this.dateTimePickerEnd.Format = DateTimePickerFormat.Time;
-            this.dateTimePickerEnd.Location = new System.Drawing.Point(12, 38);
-            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
-            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerEnd.Format = DateTimePickerFormat.Time; // Use time format
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(12, 38); // Set position
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd"; // Assign name
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(200, 20); // Set size
 
             // checkBoxBreakfast
-            this.checkBoxBreakfast.AutoSize = true;
-            this.checkBoxBreakfast.Location = new System.Drawing.Point(12, 64);
-            this.checkBoxBreakfast.Name = "checkBoxBreakfast";
-            this.checkBoxBreakfast.Size = new System.Drawing.Size(78, 17);
-            this.checkBoxBreakfast.Text = "Breakfast";
-            this.checkBoxBreakfast.UseVisualStyleBackColor = true;
+            this.checkBoxBreakfast.AutoSize = true; // Automatically size to fit content
+            this.checkBoxBreakfast.Location = new System.Drawing.Point(12, 64); // Set position
+            this.checkBoxBreakfast.Name = "checkBoxBreakfast"; // Assign name
+            this.checkBoxBreakfast.Size = new System.Drawing.Size(78, 17); // Set size
+            this.checkBoxBreakfast.Text = "Breakfast"; // Set label text
+            this.checkBoxBreakfast.UseVisualStyleBackColor = true; // Use default styling
 
             // checkBoxLunch
-            this.checkBoxLunch.AutoSize = true;
-            this.checkBoxLunch.Location = new System.Drawing.Point(12, 87);
-            this.checkBoxLunch.Name = "checkBoxLunch";
-            this.checkBoxLunch.Size = new System.Drawing.Size(68, 17);
-            this.checkBoxLunch.Text = "Lunch";
-            this.checkBoxLunch.UseVisualStyleBackColor = true;
+            this.checkBoxLunch.AutoSize = true; // Automatically size to fit content
+            this.checkBoxLunch.Location = new System.Drawing.Point(12, 87); // Set position
+            this.checkBoxLunch.Name = "checkBoxLunch"; // Assign name
+            this.checkBoxLunch.Size = new System.Drawing.Size(68, 17); // Set size
+            this.checkBoxLunch.Text = "Lunch"; // Set label text
+            this.checkBoxLunch.UseVisualStyleBackColor = true; // Use default styling
 
             // checkBoxDinner
-            this.checkBoxDinner.AutoSize = true;
-            this.checkBoxDinner.Location = new System.Drawing.Point(12, 110);
-            this.checkBoxDinner.Name = "checkBoxDinner";
-            this.checkBoxDinner.Size = new System.Drawing.Size(69, 17);
-            this.checkBoxDinner.Text = "Diner";
-            this.checkBoxDinner.UseVisualStyleBackColor = true;
+            this.checkBoxDinner.AutoSize = true; // Automatically size to fit content
+            this.checkBoxDinner.Location = new System.Drawing.Point(12, 110); // Set position
+            this.checkBoxDinner.Name = "checkBoxDinner"; // Assign name
+            this.checkBoxDinner.Size = new System.Drawing.Size(69, 17); // Set size
+            this.checkBoxDinner.Text = "Dinner"; // Set label text
+            this.checkBoxDinner.UseVisualStyleBackColor = true; // Use default styling
 
             // textBoxBreakfast
-            this.textBoxBreakfast.Location = new System.Drawing.Point(100, 64);
-            this.textBoxBreakfast.Name = "textBoxBreakfast";
-            this.textBoxBreakfast.Size = new System.Drawing.Size(100, 20);
-            this.textBoxBreakfast.Text = "30"; // default value in minutes
+            this.textBoxBreakfast.Location = new System.Drawing.Point(100, 64); // Set position
+            this.textBoxBreakfast.Name = "textBoxBreakfast"; // Assign name
+            this.textBoxBreakfast.Size = new System.Drawing.Size(100, 20); // Set size
+            this.textBoxBreakfast.Text = "30"; // Default value for breakfast
 
             // textBoxLunch
-            this.textBoxLunch.Location = new System.Drawing.Point(100, 87);
-            this.textBoxLunch.Name = "textBoxLunch";
-            this.textBoxLunch.Size = new System.Drawing.Size(100, 20);
-            this.textBoxLunch.Text = "30"; // default value in minutes
+            this.textBoxLunch.Location = new System.Drawing.Point(100, 87); // Set position
+            this.textBoxLunch.Name = "textBoxLunch"; // Assign name
+            this.textBoxLunch.Size = new System.Drawing.Size(100, 20); // Set size
+            this.textBoxLunch.Text = "30"; // Default value for lunch
 
             // textBoxDinner
-            this.textBoxDinner.Location = new System.Drawing.Point(100, 110);
-            this.textBoxDinner.Name = "textBoxDinner";
-            this.textBoxDinner.Size = new System.Drawing.Size(100, 20);
-            this.textBoxDinner.Text = "30"; // default value in minutes
+            this.textBoxDinner.Location = new System.Drawing.Point(100, 110); // Set position
+            this.textBoxDinner.Name = "textBoxDinner"; // Assign name
+            this.textBoxDinner.Size = new System.Drawing.Size(100, 20); // Set size
+            this.textBoxDinner.Text = "30"; // Default value for dinner
 
             // btnCalculate
-            this.btnCalculate.Location = new System.Drawing.Point(12, 133);
-            this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(200, 23);
-            this.btnCalculate.Text = "Start";
-            this.btnCalculate.UseVisualStyleBackColor = true;
-            this.btnCalculate.Click += new EventHandler(this.btnCalculate_Click);
+            this.btnCalculate.Location = new System.Drawing.Point(12, 133); // Set position
+            this.btnCalculate.Name = "btnCalculate"; // Assign name
+            this.btnCalculate.Size = new System.Drawing.Size(200, 23); // Set size
+            this.btnCalculate.Text = "Start"; // Button text
+            this.btnCalculate.UseVisualStyleBackColor = true; // Use default styling
+            this.btnCalculate.Click += new EventHandler(this.btnCalculate_Click); // Add click event handler
 
             // lblResult
-            this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(12, 159);
-            this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(35, 13);
-            this.lblResult.Text = "Result";
+            this.lblResult.AutoSize = true; // Automatically size to fit content
+            this.lblResult.Location = new System.Drawing.Point(12, 159); // Set position
+            this.lblResult.Name = "lblResult"; // Assign name
+            this.lblResult.Size = new System.Drawing.Size(35, 13); // Set size
+            this.lblResult.Text = "Result"; // Default label text
 
             // Form1
-            this.ClientSize = new System.Drawing.Size(340, 178);
-            this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.btnCalculate);
-            this.Controls.Add(this.textBoxDinner);
-            this.Controls.Add(this.textBoxLunch);
-            this.Controls.Add(this.textBoxBreakfast);
-            this.Controls.Add(this.checkBoxDinner);
-            this.Controls.Add(this.checkBoxLunch);
-            this.Controls.Add(this.checkBoxBreakfast);
-            this.Controls.Add(this.dateTimePickerEnd);
-            this.Controls.Add(this.dateTimePickerStart);
-            this.Name = "Form1";
-            this.Text = "Time Tracker";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            this.ClientSize = new System.Drawing.Size(340, 178); // Set form size
+            this.Controls.Add(this.lblResult); // Add result label to form
+            this.Controls.Add(this.btnCalculate); // Add calculate button to form
+            this.Controls.Add(this.textBoxDinner); // Add dinner input to form
+            this.Controls.Add(this.textBoxLunch); // Add lunch input to form
+            this.Controls.Add(this.textBoxBreakfast); // Add breakfast input to form
+            this.Controls.Add(this.checkBoxDinner); // Add dinner checkbox to form
+            this.Controls.Add(this.checkBoxLunch); // Add lunch checkbox to form
+            this.Controls.Add(this.checkBoxBreakfast); // Add breakfast checkbox to form
+            this.Controls.Add(this.dateTimePickerEnd); // Add end time picker to form
+            this.Controls.Add(this.dateTimePickerStart); // Add start time picker to form
+            this.Name = "Form1"; // Assign form name
+            this.Text = "Time Tracker"; // Set form title
+            this.ResumeLayout(false); // Resume layout updates
+            this.PerformLayout(); // Apply pending layout changes
         }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,109 +6,103 @@ namespace TimeTrackerApp
 {
     public partial class Form1 : Form
     {
-
-        private System.Windows.Forms.Timer taskTimer; // Using the timer from System.Windows.Forms
-
-        private int timeLeft; // Time remaining until the action is executed
+        private System.Windows.Forms.Timer taskTimer; // Timer to track the countdown
+        private int timeLeft; // Time remaining in seconds
 
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); // Initialize form components
 
-            //this.Size = new Size(400, 200); // Increase the window size
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.StartPosition = FormStartPosition.CenterScreen;
-           this.Icon = new Icon(@"Images/iconsailor.ico");
-           // this.Icon = new Icon(Properties.Resources.iconsailor);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog; // Disable resizing
+            this.MaximizeBox = false; // Disable maximize button
+            this.MinimizeBox = false; // Disable minimize button
+            this.StartPosition = FormStartPosition.CenterScreen; // Center the form on screen
+            this.Icon = new Icon(@"Images/iconsailor.ico"); // Set the application icon
 
-            // Create the timer
-            taskTimer = new System.Windows.Forms.Timer();
-            taskTimer.Interval = 1000; // Set the timer interval (30 seconds)
-            taskTimer.Tick += TaskTimer_Tick; // Bind the Tick event handler
+            taskTimer = new System.Windows.Forms.Timer(); // Initialize the timer
+            taskTimer.Interval = 1000; // Set the timer interval to 1 second
+            taskTimer.Tick += TaskTimer_Tick; // Assign the Tick event handler
 
-            // Create a panel
-            Panel backgroundPanel = new Panel();
-            backgroundPanel.Size = this.ClientSize; // Set the panel size to match the form's size
-            backgroundPanel.Location = new Point(0, 0);
-            //backgroundPanel.BackgroundImage = Image.FromFile(@"C:\Users\deuswork\MyWinFormsApp\backsailor.png"); // Set the panel's background image
-            backgroundPanel.BackgroundImageLayout = ImageLayout.None; // Keep the original image size
+            Panel backgroundPanel = new Panel(); // Create a panel for background
+            backgroundPanel.Size = this.ClientSize; // Match panel size with the form
+            backgroundPanel.Location = new Point(0, 0); // Set the panel location to top-left
+            backgroundPanel.BackgroundImageLayout = ImageLayout.None; // Keep original image size
 
-            // Shift the image to the right
+            // Draw the background image shifted to the right
             backgroundPanel.Paint += (s, e) =>
             {
                 e.Graphics.DrawImage(
-                    Image.FromFile(@"Images/backsailor.png"), // Image
-                    new Rectangle(250, 40, 90, 130) // Draw the image shifted to the right
+                    Image.FromFile(@"Images/backsailor.png"), // Load the background image
+                    new Rectangle(250, 40, 90, 130) // Set position and size for drawing
                 );
             };
 
             this.Controls.Add(backgroundPanel); // Add the panel to the form
-
         }
 
         private void TaskTimer_Tick(object? sender, EventArgs e)
-
         {
-            if (timeLeft > 0)
+            if (timeLeft > 0) // Check if time is remaining
             {
-                timeLeft--; // Decrease the remaining time
-                lblResult.Text = $"Time remaining: {timeLeft / 60:D2}:{timeLeft % 60:D2}"; // Format MM:SS
+                timeLeft--; // Decrement time remaining
+                lblResult.Text = $"Time remaining: {timeLeft / 60:D2}:{timeLeft % 60:D2}"; // Display time in MM:SS format
             }
             else
             {
-                taskTimer.Stop(); // Stop the timer
-                lblResult.Text = "Completed!";
-                Application.SetSuspendState(PowerState.Suspend, true, false); // Put the system into sleep mode
+                taskTimer.Stop(); // Stop the timer when finished
+                lblResult.Text = "Completed!"; // Show completion message
+                Application.SetSuspendState(PowerState.Suspend, true, false); // Put the system to sleep
             }
         }
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            DateTime startTime = dateTimePickerStart.Value;
-            DateTime endTime = dateTimePickerEnd.Value;
-            TimeSpan breakTime = TimeSpan.Zero;
+            DateTime startTime = dateTimePickerStart.Value; // Get start time
+            DateTime endTime = dateTimePickerEnd.Value; // Get end time
+            TimeSpan breakTime = TimeSpan.Zero; // Initialize total break time
 
-            // Breaks (considering the user-provided values)
+            // Add breakfast time if checked and valid
             if (checkBoxBreakfast.Checked && int.TryParse(textBoxBreakfast.Text, out int breakfastTime))
             {
                 breakTime += TimeSpan.FromMinutes(breakfastTime);
             }
+
+            // Add lunch time if checked and valid
             if (checkBoxLunch.Checked && int.TryParse(textBoxLunch.Text, out int lunchTime))
             {
                 breakTime += TimeSpan.FromMinutes(lunchTime);
             }
+
+            // Add dinner time if checked and valid
             if (checkBoxDinner.Checked && int.TryParse(textBoxDinner.Text, out int dinnerTime))
             {
                 breakTime += TimeSpan.FromMinutes(dinnerTime);
             }
 
-            TimeSpan totalWorkTime = endTime - startTime - breakTime;
-            lblResult.Text = $"Total work time: {totalWorkTime.TotalHours:F2} hours";
+            TimeSpan totalWorkTime = endTime - startTime - breakTime; // Calculate net work time
+            lblResult.Text = $"Total work time: {totalWorkTime.TotalHours:F2} hours"; // Display total work time
 
-            // Check for work time limit
-            double workTimeLimit = 8;
-            if (totalWorkTime.TotalHours > workTimeLimit)
+            double workTimeLimit = 8; // Set work time limit in hours
+            if (totalWorkTime.TotalHours > workTimeLimit) // Check if limit exceeded
             {
                 lblResult.Text += "\nWork time limit exceeded. The computer will be shut down!";
-                ShutdownComputer();
+                ShutdownComputer(); // Shutdown the computer
             }
             else
             {
-                timeLeft = (int)totalWorkTime.TotalMinutes * 60; // Convert total time to seconds
-                taskTimer.Start(); // Start the timer
+                timeLeft = (int)totalWorkTime.TotalMinutes * 60; // Convert total work time to seconds
+                taskTimer.Start(); // Start the countdown timer
             }
-
         }
 
         private void ShutdownComputer()
         {
+            // Confirm shutdown from the user
             var result = MessageBox.Show("Are you sure you want to shut down the computer?", "Confirmation", MessageBoxButtons.YesNo);
-            if (result == DialogResult.Yes)
+            if (result == DialogResult.Yes) // If user confirms
             {
-                Thread.Sleep(5000); // Wait before shutting down
-                System.Diagnostics.Process.Start("shutdown", "/s /f /t 0");
+                Thread.Sleep(5000); // Delay before shutting down
+                System.Diagnostics.Process.Start("shutdown", "/s /f /t 0"); // Execute shutdown command
             }
         }
     }
